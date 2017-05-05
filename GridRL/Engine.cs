d85a114dd337240f8dbb00916b3e7b9200941ef9
Nullsrc <beallen529@gmail.com>
2017-05-05 16:29:33 -0500

    Added basic sprite class and sprite rendering

--- GridRL/Engine.cs
+++ GridRL/Engine.cs
@@ -13,12 +13,23 @@ namespace GridRL
     public partial class Engine : Form
     {
         public static Form form;
+        public static Sprite canvas = new Sprite();
+        public static Sprite innerCanvas = new Sprite(canvas);
+        public static Sprite innerInnerCanvas = new Sprite(innerCanvas);
 
         public Engine()
         {
+            innerCanvas.X = 100;
+            innerInnerCanvas.Y = 50;
+            innerInnerCanvas.X = 50;
             InitializeComponent();
             DoubleBuffered = true;
             form = this;
+
+            // main menu function goes here
+            // if main menu returns something, start the game loop
+            // otherwise spawn some help
+            // otherwise exit the game
         }
 
         public static void gameLoop()
@@ -27,6 +38,10 @@ namespace GridRL
             // update player
             // update enemies
             // render
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            canvas.Render(e.Graphics);
         }
     }
 }
---
+++ GridRL/Sprite.cs
@@ -0,0 +1,93 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+
+namespace GridRL
+{
+    public enum ActingType { AllChildren, ThisOnly, Inactive }
+    public enum DrawingType { AllChildren, ThisOnly, Invisible}
+    public class Sprite
+    {
+        /* Constructors */
+        public Sprite() { }
+
+        public Sprite(Sprite parent) {
+            parent.AddChild(this);
+        }
+
+
+        /* Properties */
+        protected List<Sprite> children = new List<Sprite>();
+        protected Sprite parent = null;
+        protected double x = 0;
+        protected double y = 0;
+        protected double scale = 1;
+        private ActingType actingType = ActingType.AllChildren;
+        private DrawingType drawingType = DrawingType.AllChildren;
+
+        public double X {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y {
+            get { return y; }
+            set { y = value; }
+        }
+        public double Scale {
+            get { return scale; }
+            set { scale = value; }
+        }
+        public Sprite Parent {
+            get { return parent; }
+            set { parent = value; }
+        }
+        public DrawingType DrawingType {
+            get { return drawingType; }
+            set { drawingType = value; }
+        }
+        public ActingType ActingType {
+            get { return actingType; }
+            set { actingType = value; }
+        }
+
+
+        /* Children/Parent methods */
+        protected virtual void AddChild(Sprite s) {
+            s.parent = this;
+            children.Add(s);
+        }
+        protected virtual void RemoveChild(Sprite s) {
+            s.parent = null;
+            children.Remove(s);
+        }
+
+
+        /* Update/Render methods */
+        protected virtual void Act() { }
+        protected virtual void Paint(Graphics g) { g.DrawRectangle(Pens.Aqua, (float)0, (float)0, 100, 100); }
+
+        public void Update() {
+            if(actingType <= ActingType.ThisOnly) {
+                Act();
+                if(actingType == ActingType.AllChildren) {
+                    foreach(Sprite s in children) {
+                        s.Update();
+                    }
+                }
+            }
+        }
+
+        public void Render(Graphics g) {
+            if(drawingType <= DrawingType.ThisOnly) {
+                Matrix original = g.Transform.Clone();
+                g.TranslateTransform((float)x, (float)y);
+                g.ScaleTransform((float)scale, (float)scale);
+                Paint(g);
+                foreach(Sprite s in children) {
+                    s.Render(g);
+                }
+                g.Transform = original;
+            }
+        }
+    }
+}
