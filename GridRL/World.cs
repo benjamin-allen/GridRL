7a6e7538b5d050480270cf9637975aaac9163d32
Nullsrc <beallen529@gmail.com>
2017-05-08 00:17:29 -0500

    Internal modifiactions to World and Tile
    World now behaves as a giant sprite in which tile data is stored and can be rendered. Tile now holds coordinates for its position in world.

--- GridRL/World.cs
+++ GridRL/World.cs
@@ -1,7 +1,9 @@
-﻿namespace GridRL {
+﻿using System.Drawing;
+
+namespace GridRL {
 
     /// <summary> A class containing an array of tiles and a level counter. </summary>
-    public class World {
+    public class World : Sprite{
         /* Constructors */
         public World() { GenerateLevel(); }
 
@@ -21,17 +23,8 @@
         /// <summary> Deletes the current level data and creates a new one. </summary>
         /// TODO: slightly unsafe if lots of generation happens.
         public virtual void GenerateLevel() {
-            for(int y = 0; y < Program.tilesHigh - 5; ++y) {
-                for(int x = 0; x < Program.tilesWide - 16; ++x) {
-                    Program.canvas.Remove(Data[y, x]);
-                }
-            }
-            for(int y = 0; y < Program.tilesHigh - 5; ++y) {
-                for(int x = 0; x < Program.tilesWide - 16; ++x) {
-                    Tile empty = new Tile(Properties.Resources.Empty, x * Program.spriteWidth, y * Program.spriteHeight);
-                    Data[y, x] = empty;
-                }
-            }
+            Program.canvas.Remove(this);
+            Data = new Tile[Data.GetLength(0), Data.GetLength(1)];
             for(int i = 0; i < 10; ++i) {
                 int roomX = Engine.rand.Next(0, 53); // max width of room is 10
                 int roomY = Engine.rand.Next(0, 29); // max height of room is 10
@@ -40,18 +33,36 @@
                 for(int y = roomY; y <= roomY + roomH; ++y) {
                     for(int x = roomX; x <= roomX + roomW; ++x) {
                         if((y != roomY && y != roomY + roomH) && (x != roomX && x != roomX + roomW)) {
-                            Tile floor = new Tile(Properties.Resources.Empty, x * Program.spriteWidth, y * Program.spriteHeight);
+                            Tile floor = new Tile(Properties.Resources.Empty, x, y);
                             Data[y, x] = floor;
                             continue;
                         }
-                        Tile wall = new GridRL.Tile(Properties.Resources.At, x * Program.spriteWidth, y * Program.spriteHeight);
+                        Tile wall = new Tile(Properties.Resources.At, x, y);
                         Data[y, x] = wall;
                     }
                 }
             }
-            for(int y = 0; y < Program.tilesHigh - 5; ++y) {
-                for(int x = 0; x < Program.tilesWide - 16; ++x) {
-                    Program.canvas.Add(Data[y, x]);
+            Program.canvas.Add(this);
+        }
+
+        /* Overrides */
+
+        protected override void Act() {
+            for(int y = 0; y < Data.GetLength(0); ++y) {
+                for(int x = 0; x < Data.GetLength(1); ++x) {
+                    if(Data[y, x] != null) {
+                        Data[y, x].Update();
+                    }
+                }
+            }
+        }
+
+        protected override void Paint(Graphics g) {
+            for(int y = 0; y < Data.GetLength(0); ++y) {
+                for(int x = 0; x < Data.GetLength(1); ++x) {
+                    if(Data[y, x] != null) {
+                        Data[y, x].Render(g);
+                    }
                 }
             }
         }
