b950b60dd3a07e05f1a52c0a9cb7c26c9c3c4897
Nullsrc <beallen529@gmail.com>
2017-05-14 00:05:37 -0500

    Document Player.cs

--- GridRL/Player.cs
+++ GridRL/Player.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 namespace GridRL {
     public class Player : Creature {
         /* Constructors */
+        /// <summary> Creates a controllable creature. </summary>
+        /// <param name="y"> The Y coordinate to place the player in the world. </param>
+        /// <param name="x"> The X coordinate to place the player in the world. </param>
         public Player(int y, int x) : base(Properties.Resources.Player, y, x) {
             Name = "player";
             Description = "You don't have a mirror...";
@@ -15,22 +18,29 @@ namespace GridRL {
         }
 
         /* Methods */
+        /// <summary> Top-level function to capture and pass input to their sub-functions. </summary>
+        /// <param name="e"> The KeyEventArgs thing. </param>
+        /// <returns> A boolean indicating whether to advance the game. </returns>
         public bool HandleGameInput(KeyEventArgs e) {
             if(e.KeyCode == Keys.Up || e.KeyCode == Keys.NumPad8 || e.KeyCode == Keys.Down || e.KeyCode == Keys.NumPad2
             || e.KeyCode == Keys.Left || e.KeyCode == Keys.NumPad4 || e.KeyCode == Keys.Right || e.KeyCode == Keys.NumPad6) {
                 Direction dir = KeyPressToDirection(e);
+                // Check if access is possible from this actor
                 if(CanAccess(dir)) {
                     List<int> points = DirectionToPoints(dir);
+                    // Check for collision with any creatures
                     foreach(Creature c in Program.world.Creatures) {
                         if(c != this && WillCollideWith(c) && c.CoordY == CoordY && c.CoordX == CoordX) {
                             c.OnCollide(this);
                             return true;
                         }
                     }
+                    // Check for collision with world structures
                     if(WillCollideWith(Program.world.Data[points[0], points[1]])) {
                         Program.world.Data[points[0], points[1]].OnCollide(this);
                         return true;
                     }
+                    // Check to see if the world structure can be walked on
                     else if(Program.world.Data[points[0], points[1]].IsWalkable) {
                         CoordY = points[0];
                         CoordX = points[1];
@@ -42,7 +52,9 @@ namespace GridRL {
             return false;
         }
 
-
+        /// <summary> Converts a keypress to a direction. </summary>
+        /// <param name="e"> KeyEventArgs thingy. </param>
+        /// <returns> The direction to go. </returns>
         private Direction KeyPressToDirection(KeyEventArgs e) {
             if(e.KeyCode == Keys.Up || e.KeyCode == Keys.NumPad8) {
                 return Direction.Up;
