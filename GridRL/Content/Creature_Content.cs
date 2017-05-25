a1330c50cc062c1d881b6e8d77f9da65ee2ec4d7
Nullsrc <beallen529@gmail.com>
2017-05-25 02:24:09 -0500

    New creature skeledoot

--- GridRL/Content/Creature_Content.cs
+++ GridRL/Content/Creature_Content.cs
@@ -3,14 +3,21 @@ namespace GridRL {
 
     public partial class Program : Engine {
         private static Creature dummy = new Creature(Properties.Resources.Dummy);
+        private static Creature mrbones = new Creature(Properties.Resources.Skeleton);
 
         static void InitializeCreatures() {
             dummy.Name = "Dummy";
-            dummy.Description = "A mobile training dummy.";
+            dummy.Description = "A mobile training dummy. Like you, but nakeder.";
             dummy.DeathMessage = "The " + dummy.Name + "dies!";
             dummy.Health = 20;
             dummy.Visibility = Vis.Unseen;
             MasterCreatures.Add(dummy);
+
+            mrbones.Name = "Skeledoot";
+            mrbones.Description = "I want to get off Mr Bones' Wild Ride.\n:(";
+            mrbones.DeathMessage = "2spooky";
+            mrbones.Health = 30;
+            MasterCreatures.Add(mrbones);
         }
     }
 }
--- GridRL/Properties/Resources.Designer.cs
+++ GridRL/Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace GridRL.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Skeleton {
+            get {
+                object obj = ResourceManager.GetObject("Skeleton", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Stair_Down {
             get {
                 object obj = ResourceManager.GetObject("Stair_Down", resourceCulture);
